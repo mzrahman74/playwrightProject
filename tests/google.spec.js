73fcfb1897eecbdc16fef8ed875e21e980d38ac4
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2024-05-27 15:53:56 -0500

    updated

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -5,3 +5,29 @@ test("page titel", async ({ page }) => {
   console.log(page.title);
   await expect(page).toHaveTitle("Google");
 });
+
+test("webapp title & screenshot", async ({ page }) => {
+  await page.goto("https://webgopros.com");
+  console.log(page.title);
+  await expect(page).toHaveURL("https://webgopros.com");
+  await expect(page.locator("#id-footer")).toBeVisible();
+  await expect(page.locator('footer[id="id-footer" ] p')).toBeVisible();
+});
+
+test("web confirm about page assertion", async ({ page }) => {
+  await page.goto("https://webgopros.com");
+  await page.locator('a[href="/about"]').click();
+  await expect(page.locator("img.profile")).toBeVisible();
+  await expect(page.locator("#id-footer")).toBeVisible();
+  await expect(page.locator('footer[id="id-footer" ] p')).toBeVisible();
+});
+
+test("web confirm contact page assertion", async ({ page }) => {
+  await page.goto("https://webgopros.com");
+  await page.locator('a[href="/contact"]').click();
+  await expect(page.locator("img.contact_me")).toBeVisible();
+  await expect(page.locator("#note-id-2nd")).toHaveAttribute("class", "note");
+  await expect(page.locator("#note-id-3rd")).toBeVisible();
+  await expect(page.locator("#id-footer")).toBeVisible();
+  await expect(page.locator('footer[id="id-footer" ] p')).toBeVisible();
+});
